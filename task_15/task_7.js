f763d48bb9d0113eca1a84fb1387a50d5d675716
TanyaLevchenya <68747639+TanyaLevchenya@users.noreply.github.com>
2020-09-21 12:26:09 +0300

    corection task_15

--- task_15/task_7.js
+++ task_15/task_7.js
@@ -16,4 +16,13 @@ while (i < numbers.length) {
     i++;
 }
 
+let a = +prompt();
+let sum = 1;
+while (a > 0) {
+    sum *= a % 10;
+    a = (a - a % 10) / 10;
+    
+}
+console.log(sum);
+
 document.getElementsByClassName('result')[0].innerHTML = sum;
