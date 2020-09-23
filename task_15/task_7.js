c479e07985c0b0679526ecdf4540701ab138d5da
TanyaLevchenya <68747639+TanyaLevchenya@users.noreply.github.com>
2020-09-23 22:46:11 +0300

    correction task_15

--- task_15/task_7.js
+++ task_15/task_7.js
@@ -1,27 +1,26 @@
 'use strict'
-let result;
+// let result;
 
-let numbers = prompt();
-let sum = 0;
-console.log(numbers);
+// let numbers = prompt();
+// let sum = 0;
+// console.log(numbers);
 
-let i = 0;
-while (i < numbers.length) {
-    if(i == 0) {
-        sum = +numbers[i];
-    } else {
-        sum *= +numbers[i];
-        console.log(sum)
-    }
-    i++;
-}
+// let i = 0;
+// while (i < numbers.length) {
+//     if(i == 0) {
+//         sum = +numbers[i];
+//     } else {
+//         sum *= +numbers[i];
+//         console.log(sum)
+//     }
+//     i++;
+// }
 
 let a = +prompt();
 let sum = 1;
 while (a > 0) {
     sum *= a % 10;
     a = (a - a % 10) / 10;
-    
 }
 console.log(sum);
 
