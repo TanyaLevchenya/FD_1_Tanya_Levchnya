d0938afcd8956840c8f78b73cb5a33591e5bca7b
TanyaLevchenya <68747639+TanyaLevchenya@users.noreply.github.com>
2020-10-09 11:23:23 +0300

    create project

--- project/main.js
+++ project/main.js
@@ -1,4 +1,20 @@
 'use strict'
 
-  
-    
+window.onscroll=function(){
+    var scrollElem = document.getElementById(scrollTop);
+    if(document.body.scrollTop > documentElement.clientHeight){
+        scrollElem.arrow.opacity = '1';
+    }
+    else{
+        scrollElem.arrow.opacity = '0'; 
+    }
+};
+var timeOut;
+function goUp(){
+    var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if(top > 0){
+        window.scrollBy(0, -100);
+        timeOut = setTimeout('goUp()', 20);
+    }
+    else clearTimeout(timeOut);
+}
